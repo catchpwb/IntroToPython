8756b60dafb5795b6d547ae2db0e83f97a24a4fa
Will <catchpwb@gmail.com>
2025-09-10 14:00:35 -0400

    added exercises

--- 8.23.py
+++ 8.23.py
@@ -4,4 +4,20 @@ Created on Wed Sep 10 13:50:56 2025
 
 @author: Will
 """
-
+"""
+Develop a class BankAccount that supports these methods:
+• __init__(): Initializes the bank account balance to the value of the input argument,
+or to 0 if no input argument is given
+• withdraw(): Takes an amount as input and withdraws it from the balance
+• deposit(): Takes an amount as input and adds it to the balance
+• balance(): Returns the balance on the account
+>>> x = BankAccount(700)
+>>> x.balance())
+700.00
+>>> x.withdraw(70)
+>>> x.balance()
+630.00
+>>> x.deposit(7)
+>>> x.balance()
+637.00
+"""
