8c2d32b173a626812f40ead37c6ce94ad2665555
Will <catchpwb@gmail.com>
2025-09-10 14:41:57 -0400

    added exercises

--- 8.23.py
+++ 8.23.py
@@ -5,6 +5,7 @@ Created on Wed Sep 10 13:50:56 2025
 @author: Will
 """
 """
+page 325
 Develop a class BankAccount that supports these methods:
 • __init__(): Initializes the bank account balance to the value of the input argument,
 or to 0 if no input argument is given
@@ -21,3 +22,31 @@ or to 0 if no input argument is given
 >>> x.balance()
 637.00
 """
+
+class BankAccount:
+    'represents a bank account with withdraw, deposit, balance methods'
+
+    def __init__(self, initial = 0):
+        'initialize balance'
+        self.initial = initial
+
+    def withdraw(self, takeOut):
+        'withdraw from account'
+        self.initial -= takeOut
+
+    def deposit(self, putIn):
+        'deposit money into account'
+        self.initial += putIn
+
+    def balance(self):
+        'check remaining balance'
+        print(self.initial)
+
+
+x = BankAccount(700)
+x.balance()
+x.withdraw(70)
+x.balance()
+x.deposit(7)
+x.balance()
+help(BankAccount)
